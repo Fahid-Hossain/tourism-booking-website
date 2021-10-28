5b12ae03481c7d0cef01d66a21e57d40fa03740a
Fahid Hossain <fahidhossain2021@gmail.com>
2021-10-29 01:38:40 +0600

    initial setup with route

---
+++ src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand>React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Link className="mx-2 text-decoration-none text-white nav-link" to="/home">Home</Link>
+                            <Link className="mx-2 text-decoration-none text-white nav-link" to="/about">About</Link>
+                            <Link className="mx-2 text-decoration-none text-white nav-link" to="/tours">Tours</Link>
+                            <Link className="mx-2 text-decoration-none text-white nav-link" to="contact">Contact</Link>
+                        </Nav>
+                        <Nav>
+                            <Link className="mx-2 text-decoration-none text-white nav-link" to="/signIn">Sign In</Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Tours/Tours.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tours = () => {
+    return (
+        <div>
+            <h1>Your tours</h1>
+        </div>
+    );
+};
+
+export default Tours;
