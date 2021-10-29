72862fbdc3193eff3bff566c690a28646addcc50
Fahid Hossain <fahidhossain2021@gmail.com>
2021-10-29 16:30:56 +0600

    react hook form setup

--- src/components/AddTrip/AddTrip.js
+++ src/components/AddTrip/AddTrip.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import "./AddTrip.css"
 
 const AddTrip = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
     return (
-        <div>
+        <div className="add-trip">
             <h1>Add a Trip You Like</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input placeholder="Destination" {...register("name", { required: true, maxLength: 20 })} />
+                <textarea placeholder="short description" {...register("description")} />
+                <input placeholder="price" type="number" {...register("age")} />
+                <input placeholder="img url" {...register("img")} />
+                <input type="submit" />
+            </form>
         </div>
     );
 };
